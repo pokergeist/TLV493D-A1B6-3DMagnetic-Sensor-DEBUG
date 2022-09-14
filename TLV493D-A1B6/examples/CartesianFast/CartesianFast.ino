d0f2b980b73a9b0e6eb86c6c8b632d2781424006
John J <33000716+pokergeist@users.noreply.github.com>
2022-09-14 08:15:42 -0400

    added free memory test

--- TLV493D-A1B6/examples/CartesianFast/CartesianFast.ino
+++ TLV493D-A1B6/examples/CartesianFast/CartesianFast.ino
@@ -1,7 +1,11 @@
 #include <Tlv493d.h>
+#include <util/free_mem.h>
 
 // Tlv493d Opject
 Tlv493d Tlv493dMagnetic3DSensor = Tlv493d();
+
+uint32_t line_counter;
+int      free_mem, free_mem_min;
 
 void setup() {
   Serial.begin(9600);
@@ -15,9 +19,21 @@ void setup() {
   Tlv493dMagnetic3DSensor.begin();
   Tlv493dMagnetic3DSensor.setAccessMode(Tlv493dMagnetic3DSensor.MASTERCONTROLLEDMODE);
   Tlv493dMagnetic3DSensor.disableTemp();
+  free_mem_min = freeMemory();
 }
 
 void loop() {
+  if (line_counter++ % 256 == 0) {
+    free_mem = freeMemory();
+    if (free_mem < free_mem_min) {
+      Serial.print("Free memory:");
+      Serial.print(free_mem);
+      Serial.print(" ***********************************");
+      free_mem_min = free_mem;
+      Serial.println();
+      delay(2e3);
+    }
+  }
   delay(Tlv493dMagnetic3DSensor.getMeasurementDelay());
   Tlv493d_Error read_status = Tlv493dMagnetic3DSensor.updateData();
 
